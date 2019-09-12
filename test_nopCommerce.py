a6db575f4d21aa7c86ff9799df6be7cf017d4c87
Przemyslaw <pk4btc@gmail.com>
2019-09-12 02:50:30 +0200

    checking title

--- test_nopCommerce.py
+++ test_nopCommerce.py
@@ -1,8 +1,8 @@
 # C:\Users\live4\Desktop\My projects\selScripts
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import unittest
+import os
 
 
 
@@ -13,41 +13,25 @@ class testMainPage(unittest.TestCase):
         self.driver = webdriver.Chrome()
 
 
-
-    def test_search_in_python_org(self):
-        driver = self.driver
-        driver.get("https://www.nopcommerce.com/")
-
-        self.assertIn("nopCommerce - ASP.NET Open-source Ecommerce Shopping Cart Solution", driver.title)
-
-
-    def tearDown(self):
-        self.driver.close()
-
     def test_chekingForm(self):
         driver = self.driver
         driver.get("https://www.nopcommerce.com/")
-
-        formElem=driver.find_element_by_id("newsletter-email")
-        formElem.send_keys("Adas")
-
-        formElem2=driver.find_element_by_name("Email")
-        formElem2.send_keys("amen@wp.com")
-
-        formElem3=page.find_element_by_name("Enquiry")
-        formElem3.send_keys("Help to recive new  orders from shop")
-
-        formElemSubmit=driver.find_element_by_name("send-email")
-        formElemSubmit.submit()
+        driver.implicitly_wait(3)  # seconds
+        formElem2 =driver.find_element_by_class_name("facebook")
+        formElem2.click()
 
 
+    def test_checkingTitle(self):
+        driver = self.driver
+        driver.implicitly_wait(3)  # seconds
+        page=driver.title
+        self.assertEqual('',page)
+        driver.implicitly_wait(3)  # seconds
+        print(driver.title)
 
+    def tearDown(self):
 
-
-
-
-
-
+        self.driver.close()
 
 
 
