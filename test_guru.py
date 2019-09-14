300b1e8462e2b99ce0d7c1f12f17521973969b01
Przemyslaw <pk4btc@gmail.com>
2019-09-14 03:53:34 +0200

    test logo go back

--- test_guru.py
+++ test_guru.py
@@ -52,6 +52,20 @@ class testMainPage(unittest.TestCase):
         select.select_by_index('2')
 
 
+    def test_if_logo_goback(self):
+        driver = self.driver
+        driver.get("https://www.nopcommerce.com/shoppingcart.aspx")
+        logo = driver.find_element_by_css_selector('div.header-logo a img')
+        src_base=logo.tag_name
+        logo.click()
+
+        print(f'znalezione za pomoca {src_base}')
+        if driver.title == 'nopCommerce - ASP.NET Open-source Ecommerce Shopping Cart Solution':
+            print('powrot do home ')
+
+        else:print('fail')
+
+
 
 
 
