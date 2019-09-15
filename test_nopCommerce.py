8c0645a4b5666d41a1bcd6fcb9e07c2071cdec45
Przemyslaw <pk4btc@gmail.com>
2019-09-15 03:40:24 +0200

    added dragANDdrop test

--- test_nopCommerce.py
+++ test_nopCommerce.py
@@ -1,7 +1,15 @@
 # C:\Users\live4\Desktop\My projects\selScripts
 
 from selenium import webdriver
+
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
 import unittest
+from selenium.webdriver import ActionChains
+
 import os
 
 
@@ -43,6 +51,33 @@ class testMainPage(unittest.TestCase):
             print('powrot do home ')
 
         else:print('fail')
+
+    def test_DragDrop(self):
+        driver= self.driver
+        wait = WebDriverWait(driver, 10)
+        driver.get("http://listmoz.com/")
+        driver.implicitly_wait(1)  # seconds
+        x=1
+        while x < 10:
+            driver.implicitly_wait(2)  # seconds
+            driver.find_element_by_name("add").send_keys(f'Eat {x}')
+            driver.find_element_by_id('submit').click()
+            x=x+1
+        driver.implicitly_wait(2)  # seconds
+        WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.XPATH, '/html/body/div/ul/li[1]')))
+        # time.sleep(3)
+        first = driver.find_element_by_xpath('/html/body/div/ul/li[1]')
+        put =  driver.find_element_by_xpath('/html/body/div/ul/li[2]')
+        put.click()
+        action_chains = ActionChains(driver)
+        action_chains.drag_and_drop(first,put).perform()
+        driver.implicitly_wait(1)  # seconds
+
+
+
+
+
 
 
     def tearDown(self):
